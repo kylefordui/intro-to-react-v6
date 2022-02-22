2503804bb0800e8656aa79dab918288fb3c6c27e
Kyle <contact@kylethomas.com.au>
2022-02-22 21:27:11 +1100

    Commit test.

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -21,7 +21,6 @@ const SearchParams = () => {
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
-
     setPets(json.pets);
   }
 
