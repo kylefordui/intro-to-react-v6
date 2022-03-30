7afa2acf191e40f70c7c025ac95ce6554d62394e
Kyle <kylefordui@gmail.com>
2022-03-30 14:41:01 +1100

    Add react routes.

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,28 @@
+import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+        <Route path="/">
+          <SearchParams />
+        </Route>
+      </Router>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
